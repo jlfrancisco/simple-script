fe0710febceb017d98e80b6bacecf15097c9063e
Jean Luc F SEMEDO <jsemedo_ct@securiport.com>
2023-04-14 22:28:18 -0400

    first commit

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM python:latest 
+COPY main.py main.py 
+ENTRYPOINT [ "python", "main.py" ]
---
+++ main.py
@@ -0,0 +1,12 @@
+import sys 
+
+def printhello():
+    print("Hello")
+
+def calculate(a, b):
+    return int(a)+int(b)
+
+if __name__=="__main__":
+    arg1, arg2 = sys.argv[1:3]
+    printhello()
+    print(calculate(arg1, arg2))
