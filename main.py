0669b5156a6dc2f89d0b766b43b05ac8448bdb6e
Jean Luc F SEMEDO <jsemedo_ct@securiport.com>
2023-04-17 18:20:50 -0400

    add python toolkit for github actions

--- main.py
+++ main.py
@@ -12,4 +12,4 @@ if __name__=="__main__":
     arg1, arg2 = sys.argv[1:3]
     #printhello()
     print(f"result={calculate(arg1, arg2)}")
-    core.set_ouput("result", calculate(arg1, arg2))
+    core.set_output("result", calculate(arg1, arg2))
